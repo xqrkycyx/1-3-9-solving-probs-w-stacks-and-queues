a218f7564c8aba740e80cbf8848514a1dca7b8a3
Will Williams <wwilliams@indeed.com>
2024-02-27 17:57:20 -0800

    Implements evaluate using Stack

--- src/postfix.js
+++ src/postfix.js
@@ -1,3 +1,37 @@
-const evaluate = (expression) => {};
+const Stack = require("./lib/stack");
+
+const evaluate = (expression) => {
+  let stack = new Stack();
+
+  // Removing white spaces from the expression
+  expression = expression.replace(/\s/g, "");
+
+  expression.split("").forEach((character) => {
+    // Checking if the character is a number
+    if (!"+-*/".includes(character)) {
+      stack.push(parseInt(character));
+    }
+
+    // Checking if the character is an operator (+, -, *, /)
+
+    if ("+-*/".includes(character)) {
+      let a = stack.pop();
+      let b = stack.pop();
+
+      const mathMachine = {
+        "+": b + a,
+        "-": b - a,
+        "/": b / a,
+        "*": b * a,
+      };
+
+      const result = mathMachine[character];
+
+      stack.push(parseInt(result));
+    }
+  });
+
+  return stack.pop();
+};
 
 module.exports = evaluate;
