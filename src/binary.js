880cadc7f2688a9d00e429a4afc3ccbee67b0141
Will Williams <wwilliams@indeed.com>
2024-02-27 17:52:11 -0800

    Implements binary using Queue

--- src/binary.js
+++ src/binary.js
@@ -1,3 +1,25 @@
-const binary = (max) => {};
+const Queue = require("./lib/queue");
+
+const binary = (max) => {
+  const queue = new Queue();
+
+  queue.enqueue("1");
+
+  let result = [];
+
+  for (let i = 0; i < max; i++) {
+    // Dequeue
+    const value = queue.dequeue();
+
+    // Push
+    result.push(value);
+
+    // updated here
+    queue.enqueue(value.concat("0"));
+
+    queue.enqueue(value.concat("1"));
+  }
+  return result;
+};
 
 module.exports = binary;
