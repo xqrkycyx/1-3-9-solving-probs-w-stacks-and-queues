674f493d244e681362fcbc941a8d61b768fd057f
Will Williams <wwilliams@indeed.com>
2024-02-27 17:55:13 -0800

    Implements brackets using Stack

--- src/brackets.js
+++ src/brackets.js
@@ -1,5 +1,30 @@
 const Stack = require("./lib/stack");
 
-const match = (expression) => {};
+const match = (expression) => {
+  const stack = new Stack();
+
+  for (let index = 0, limit = expression.length; index < limit; index++) {
+    const character = expression[index];
+    const closingBracket = {
+      "(": ")",
+      "{": "}",
+      "[": "]",
+    };
+    // if opening bracket, then push the corresponding closing bracket into stack
+    if ("({[".includes(character)) {
+      stack.push(closingBracket[character]);
+      // if closing bracket, top of the stack must match (then pop) or else brackets are out of order.
+    } else {
+      if (")}]".includes(character)) {
+        if (stack.top?.value === character) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+  return !stack.top;
+};
 
 module.exports = match;
